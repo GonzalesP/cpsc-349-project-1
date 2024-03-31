35f25da871ab519de4bd1cef611ef4eeb631e089
P Gonz <GonzalesP@csu.fullerton.edu>
2024-03-31 07:03:36 -0700

    Integrated Eric's method of separating CSS and JSON files. TODO: integrate randomization and quiz menu.

---
+++ questions.js
@@ -0,0 +1,290 @@
+// this JS file contains the JSONs of all quizzes
+// when a quiz is rendered in quiz.html, its JSON is "fetched" from this file
+
+// each quiz is represented as a JSON array of objects,
+// and each object represents a question.
+// every question has a 'prompt', an 'answer', and a 'choices' object.
+
+// prompt is the question itself (e.g. "What is 2+2?")
+// choices stores key-value pairs for each choice and value (e.g. A: 4)
+// answer indicates which choice is correct
+
+
+
+// JSON for Quiz 1 - 12 total questions (10 are selected)
+var quiz1 = [
+  {
+    prompt: "What a day keeps the doctor away?",
+    choices: {
+      A: "Apple",
+      B: "Banana",
+      C: "Cherry",
+      D: "Date",
+    },
+    answer: "A"
+  },
+  {
+    prompt: "What color is the sky during the day?",
+    choices: {
+      A: "Blue",
+      B: "Green",
+      C: "Purple",
+      D: "Red",
+    },
+    answer: "A"
+  },
+  {
+    prompt: "How big is Stewart?",
+    choices: {
+      A: "Little",
+      B: "Big",
+      C: "Tiny",
+      D: "Giant",
+    },
+    answer: "A"
+  },
+  {
+    prompt: "What animal has 9 lives?",
+    choices: {
+      A: "Cat",
+      B: "Dog",
+      C: "Bird",
+      D: "Lizard",
+    },
+    answer: "A"
+  },
+  {
+    prompt: "What year is it?",
+    choices: {
+      A: "2021",
+      B: "2022",
+      C: "2023",
+      D: "2024",
+    },
+    answer: "D"
+  },
+  {
+    prompt: "Which Quiz is this?",
+    choices: {
+      A: "Quiz 1",
+      B: "Quiz 2",
+      C: "Quiz 3",
+      D: "Quiz 4",
+    },
+    answer: "A"
+  },
+  {
+    prompt: "Who is the school mascot?",
+    choices: {
+      A: "Tony the Tiger",
+      B: "Tuffy the Titan",
+      C: "Sonic the Hedgehog",
+      D: "Courage the Cowardly Dog",
+    },
+    answer: "B"
+  },
+  {
+    prompt: "Which of these is a state?",
+    choices: {
+      A: "Washington D.C",
+      B: "East Dakota",
+      C: "North Virginia",
+      D: "Rhode Island",
+    },
+    answer: "D"
+  },
+  {
+    prompt: "What kind of fish is Nemo?",
+    choices: {
+      A: "Catfish",
+      B: "Sandfish",
+      C: "Clownfish",
+      D: "Flyfish",
+    },
+    answer: "C"
+  },
+  {
+    prompt: "The answer is B",
+    choices: {
+      A: "Blue",
+      B: "Green",
+      C: "Purple",
+      D: "Red",
+    },
+    answer: "B"
+  },
+  {
+    prompt: "The answer is D",
+    choices: {
+      A: "Blue",
+      B: "Green",
+      C: "Purple",
+      D: "Red",
+    },
+    answer: "D"
+  },
+  {
+    prompt: "The answer is C",
+    choices: {
+      A: "Blue",
+      B: "Green",
+      C: "Purple",
+      D: "Red",
+    },
+    answer: "C"
+  },
+];
+
+// JSON for Quiz 2 - 15 total questions (10 are selected)
+var quiz2 = [
+  {
+    prompt: "0+1=",
+    choices: {
+      A: "1",
+      B: "2",
+      C: "3",
+      D: "4",
+    },
+    answer: "A"
+  },
+  {
+    prompt: "1+2=",
+    choices: {
+      A: "1",
+      B: "2",
+      C: "3",
+      D: "4",
+    },
+    answer: "C"
+  },
+  {
+    prompt: "1+1=",
+    choices: {
+      A: "1",
+      B: "2",
+      C: "3",
+      D: "4",
+    },
+    answer: "B"
+  },
+  {
+    prompt: "2+2=",
+    choices: {
+      A: "1",
+      B: "2",
+      C: "3",
+      D: "4",
+    },
+    answer: "D"
+  },
+  {
+    prompt: "1*1=",
+    choices: {
+      A: "1",
+      B: "2",
+      C: "3",
+      D: "4",
+    },
+    answer: "A"
+  },
+  {
+    prompt: "1*2=",
+    choices: {
+      A: "1",
+      B: "2",
+      C: "3",
+      D: "4",
+    },
+    answer: "B"
+  },
+  {
+    prompt: "2*2=",
+    choices: {
+      A: "1",
+      B: "2",
+      C: "3",
+      D: "4",
+    },
+    answer: "D"
+  },
+  {
+    prompt: "2/2=",
+    choices: {
+      A: "1",
+      B: "2",
+      C: "3",
+      D: "4",
+    },
+    answer: "A"
+  },
+  {
+    prompt: "4-3=",
+    choices: {
+      A: "1",
+      B: "2",
+      C: "3",
+      D: "4",
+    },
+    answer: "A"
+  },
+  {
+    prompt: "4-2=",
+    choices: {
+      A: "1",
+      B: "2",
+      C: "3",
+      D: "4",
+    },
+    answer: "B"
+  },
+  {
+    prompt: "4-1",
+    choices: {
+      A: "1",
+      B: "2",
+      C: "3",
+      D: "4",
+    },
+    answer: "C"
+  },
+  {
+    prompt: "4-0=",
+    choices: {
+      A: "1",
+      B: "2",
+      C: "3",
+      D: "4",
+    },
+    answer: "D"
+  },
+  {
+    prompt: "3-(-1)=",
+    choices: {
+      A: "1",
+      B: "2",
+      C: "3",
+      D: "4",
+    },
+    answer: "D"
+  },
+  {
+    prompt: "2-(-2)=",
+    choices: {
+      A: "1",
+      B: "2",
+      C: "3",
+      D: "4",
+    },
+    answer: "D"
+  },
+  {
+    prompt: "1-(-2)=",
+    choices: {
+      A: "1",
+      B: "2",
+      C: "3",
+      D: "4",
+    },
+    answer: "C"
+  } 
+];
